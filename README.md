95a81e221eba1860b7de52d5b3e2ec6ce1ca4c93
jachcastro <jachcastro@hotmail.com>
2023-08-08 10:10:59 -0500

    Nuevo commit después del reset

--- README.md
+++ README.md
@@ -13,3 +13,37 @@
 
 ## Introducción 
 El ciclo de vida de un proyecto de Machine Learning debe contemplar desde el tratamiento y recolección de los datos (Data Engineer stuff) hasta el entrenamiento y mantenimiento del modelo de ML según llegan nuevos datos.
+
+## Transformaciones
+En el archivo ETL se hicieron las tranformaciones solicitadas
+Conforme se iban trabajando los archivos se creo una carpeta para usarlo como fuente de datos
+1_datasets_origen, como nos entregaron los archivos
+2_datasets_etl, los csv después de las primeras transformaciones solicitadas
+3_datasets_api, adapte los CSV para las funciones
+4_datasets_eda, el resultado después de aplicar EDA
+5_datasets_ml csv necesario para hacer la recomendación
+Los campos compuestos por listas y diccionarios se separaron y se crearon nuevos archivos CSV, conforme se vaya reduciendo
+
+## Desarrollo API. 
+Localmente logramos hacer funcionar con FastApi
+Luego lo publicamos en render
+
+## Análisis exploratorio de los datos. 
+Se analizaons los campos para decidir cual es relevante para el desarrollo a realizar
+Para hacer una consulta de recomendacoón nos basaremos en dos formas
+- en base al genero y popularidad 
+- en base al titulo y contenido
+obtendremos los datasets necesarios para ser consumidos por el modelo resultante 
+
+## Sistema de recomendación
+Se implementan dos modelos
+y para el API quedaría
+
+## Enlaces
+- Video
+- API
+- Repositorio
+- Datasets 
+
+## Referencias
+Enlces de todo lo consultado
---
+++ main.py
@@ -0,0 +1,186 @@
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+
+@app.get('/peliculas_idioma/{idioma}')
+def peliculas_idioma(idioma:str):
+    '''Ingresas el idioma, retornando la cantidad de peliculas producidas en el mismo'''
+    # Se ingresa un idioma (como están escritos en el dataset, no hay que traducirlos!). Debe devolver la cantidad de películas producidas en ese idioma.
+    # Ejemplo de retorno: X cantidad de películas fueron estrenadas en idioma
+
+    # Validar si el parámetro es de tipo string
+    if not isinstance(idioma, str):
+        return "El parámetro 'idioma' debe ser un string."
+
+    # Leer el archivo CSV
+    df = pd.read_csv('3_datasets_api/spoken_data.csv')
+    
+    # Filtrar las películas que coincidan con el idioma
+    peliculas_filtradas = df[df['iso_639_1'].str.lower() == idioma.lower()]
+    
+    # Obtener la cantidad de películas para ese idioma
+    cantidad_peliculas = peliculas_filtradas['id_movie'].nunique()
+    
+    return  f"{cantidad_peliculas} numero de películas que fueron estrenadas en idioma: {idioma}"
+
+@app.get('/peliculas_duracion/{pelicula}')
+def peliculas_duracion(pelicula: str):
+    '''Ingresas la pelicula, retornando la duracion y el año'''
+
+    # Se ingresa una pelicula. Debe devolver la duracion y el año.
+    # Ejemplo de retorno: X . Duración: x. Año: xx
+
+    # Validar si el parámetro es de tipo string
+    if not isinstance(pelicula, str):
+        return "El parámetro 'pelicula' debe ser un string."
+
+    # Leer el archivo CSV que contiene la información de las películas
+    df = pd.read_csv('3_datasets_api/peliculas_duracion.csv')
+
+    # Buscar la película específica por su título en la columna 'title'
+    pelicula_encontrada = df[df['title'].str.lower() == pelicula.lower()]
+
+    if len(pelicula_encontrada) == 0:
+        return f"No se encontró la película: {pelicula}"
+
+    # Obtener la duración y el año de la película
+    duracion = pelicula_encontrada['runtime'].iloc[0]
+    año = pelicula_encontrada['release_year'].iloc[0]
+
+    return f"{pelicula}. Duración: {duracion} minutos. Año: {año}"
+
+@app.get('/franquicia/{franquicia}')
+def franquicia(Franquicia: str):
+    '''Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia total y promedio'''
+    # def franquicia( Franquicia: str ): 
+    # Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia total y promedio
+    # Ejemplo de retorno: La franquicia X posee X peliculas, una ganancia total de x y una ganancia promedio de xx
+
+    # Validar si el parámetro es de tipo string
+    if not isinstance(Franquicia, str):
+        return "El parámetro 'franquicia' debe ser un string."
+
+    # Leer los archivos CSV en DataFrames
+    franquicia_df= pd.read_csv('3_datasets_api/franquicia.csv')
+    
+    # Filtrar las películas de la franquicia
+    franquicia_df = franquicia_df[franquicia_df['name_collection'].str.lower() == Franquicia.lower()]
+    
+    # Obtener la cantidad de películas
+    cantidad_peliculas = franquicia_df.shape[0]
+    
+    # Obtener la ganancia total
+    ganancia_total = franquicia_df['revenue'].sum()/1000000
+    
+    # Obtener la ganancia promedio
+    ganancia_promedio = franquicia_df['revenue'].mean()/1000000
+    
+    # Formatear el resultado como un mensaje de retorno
+    resultado = f"La franquicia {Franquicia} posee {cantidad_peliculas} películas, con una ganancia total de {ganancia_total:,.0f} millones y una ganancia promedio de {ganancia_promedio:,.0f} millones por cada una."
+    
+    return resultado
+
+@app.get('/peliculas_pais/{pais}')
+def peliculas_pais(Pais: str):
+    '''Ingresas el pais, retornando la cantidad de peliculas producidas en el mismo'''
+    # def peliculas_pais( Pais: str ): 
+    # Se ingresa un país (como están escritos en el dataset, no hay que traducirlos!), retornando la cantidad de peliculas producidas en el mismo.
+    # Ejemplo de retorno: Se produjeron X películas en el país X
+
+    # Validar si el parámetro es de tipo string
+    if not isinstance(Pais, str):
+        return "El parámetro 'Pais' debe ser un string."
+
+    # Leer el archivo CSV de películas y países
+    countries_df = pd.read_csv('3_datasets_api/countries_data.csv')
+
+    # Filtrar las películas que coincidan con el país ingresado
+    peliculas_filtradas = countries_df[countries_df['iso_3166_1'].str.lower() == Pais.lower()]
+
+    # Contar la cantidad de películas producidas en el país
+    cantidad_peliculas = peliculas_filtradas.shape[0]
+    if cantidad_peliculas>0 :
+        pais_peliculas_filtradas = peliculas_filtradas.groupby('name_countries').first().index[0]
+    else:
+        pais_peliculas_filtradas = ''
+
+    return f"Se produjeron {cantidad_peliculas} películas en el país {pais_peliculas_filtradas}"
+
+
+@app.get('/productoras_exitosas/{productora}')
+def productoras_exitosas(Productora: str):
+    '''Ingresas la productora, entregandote el revunue total y la cantidad de peliculas que realizo '''
+    # def productoras_exitosas( Productora: str ): 
+    # Se ingresa la productora, entregandote el revunue total y la cantidad de peliculas que realizo.
+    # Ejemplo de retorno: La productora X ha tenido un revenue de x
+
+    # Validar si el parámetro es de tipo string
+    if not isinstance(Productora, str):
+        return "El parámetro 'Productora' debe ser un string."
+
+    # Leer el archivo CSV de productora exitosa
+    productoras_exitosas_df = pd.read_csv('3_datasets_api/productoras_exitosas.csv')
+
+    # Filtrar las películas que coincidan con la productora ingresada
+    productoras_exitosas_df = productoras_exitosas_df[productoras_exitosas_df['name_companies'].str.lower() == Productora.lower()]
+
+    # Calcular el total de revenue y la cantidad de películas realizadas
+    revenue_total = productoras_exitosas_df['revenue'].sum()/100000
+    cantidad_peliculas = productoras_exitosas_df['id_movie'].nunique()
+
+    return f"La productora {Productora} ha tenido un revenue total de {revenue_total:,.0f} millones y ha realizado {cantidad_peliculas} películas."
+
+
+
+@app.get('/get_director/{nombre_director}')
+def get_director(Nombre_Director: str):
+    ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
+    Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma. En formato lista'''
+    # def get_director( Nombre_Director ): 
+    # Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
+    # Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma, 
+    # en formato lista.
+
+    # Validar si el parámetro es de tipo string
+    if not isinstance(Nombre_Director, str):
+        return "El parámetro 'Nombre_Director' debe ser un string."
+
+    # Leer el archivo CSV de productora exitosa
+    director_movies_df = pd.read_csv('3_datasets_api/director_movies.csv')
+
+    # Filtrar las películas dirigidas por el director especificado
+    director_movies_df = director_movies_df[(director_movies_df['name_crew'].str.lower() == Nombre_Director.lower())]
+
+    if director_movies_df.empty:
+        return "El director no se encuentra en el dataset o no ha dirigido películas."
+
+    # Calcular el éxito del director (promedio de votos de sus películas)
+    exito_director = director_movies_df['revenue'].sum()
+    # Formatear los valores numéricos
+    exito_director = '{:,.0f}'.format(exito_director)
+
+    # Crear una lista para almacenar la información de cada película
+    peliculas_info = []
+
+    # Iterar sobre las filas del DataFrame para obtener la información requerida
+    for index, row in director_movies_df.iterrows():
+        retorno_individual = '{:,.0f}'.format(row['revenue'])
+        costo = '{:,.0f}'.format(row['budget'])
+        ganancia = '{:,.0f}'.format(row['revenue'] - row['budget'])
+        pelicula_info = {
+            'nombre': row['title'],
+            'fecha_lanzamiento': str(row['release_date']),
+            'retorno_individual': retorno_individual,
+            'costo': costo,
+            'ganancia': ganancia 
+        }
+        peliculas_info.append(pelicula_info)
+
+    return exito_director, peliculas_info
+
+# ML
+@app.get('/recomendacion/{titulo}')
+def recomendacion(titulo:str):
+    '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
+    return {'lista recomendada'}
